e112a52290f55b11c66939bc99f585073d2fe325
AndresEspositoDe <98119676+AndresEspositoDe@users.noreply.github.com>
2024-10-10 15:32:08 -0400

    Segundo commit:art:
    Arte

--- Primer-Proyecto-OS---Bucarello-Esposito/src/Main/Main.java
+++ Primer-Proyecto-OS---Bucarello-Esposito/src/Main/Main.java
@@ -2,20 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package primer.proyecto.os.bucarello.esposito;
+package Main;
 
 /**
  *
  * @author andresbucarello
  */
-public class PrimerProyectoOSBucarelloEsposito {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+        App app = App.getInstance();
+        app.start();
     }
     
 }
